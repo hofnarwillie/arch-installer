eb8e04d134d68ef683dc3692981f2dfd7dcb61ac
Willem Le Roux <willem.steynleroux@prudential.co.uk>
2020-02-08 00:26:29 +0000

    WIP

--- run.sh
+++ run.sh
@@ -4,9 +4,9 @@ set -e;
 function join { local IFS=""; echo "$*"; }
 
 # live boot system settings
-# loadkeys uk
-# timedatectl set-ntp true
-# timedatectl status
+loadkeys uk
+timedatectl set-ntp true
+timedatectl status
 
 # drive partitions
 DEVICE=/dev/sda
@@ -18,14 +18,12 @@ ANSWERS=(
     $NEWLINE      # Default to partition 1
     $NEWLINE      # Default first sector
     +30G$NEWLINE  # 30GB
-    Y$NEWLINE     # Remove existing signature
     a$NEWLINE     # Set bootable
   n$NEWLINE   # New Partition
     $NEWLINE      # Default to primary
     $NEWLINE      # Default to partition 2
     $NEWLINE      # Default first sector
     +2G$NEWLINE   # 2GB
-    Y$NEWLINE     # Remove existing signature
     type$NEWLINE  # Set type
     $NEWLINE      # select partition 2
     82$NEWLINE    # choose SWAP
@@ -34,8 +32,17 @@ ANSWERS=(
     $NEWLINE      # Default to partition 3
     $NEWLINE      # Default first sector
     $NEWLINE      # Default last sector
-    Y$NEWLINE     # Remove existing signature
   w$NEWLINE   # Write
 )
 
 printf $(join "${ANSWERS[@]}") | sudo fdisk "$DEVICE"
+
+mkfs.ext4 /dev/sda1
+mkfs.ext4 /dev/sda3
+
+mkswap /dev/sda2
+swapon /dev/sda2
+
+mount /dev/sda1 /mnt
+mkdir /mnt/home
+mount /dev/sda3 /mnt/home
